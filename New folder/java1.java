f103e265f0d428eef65b8b6f62d586e38f572d8f
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-08-11 10:19:19 +0530

    Add files via upload

---
+++ New folder/java1.java
@@ -0,0 +1,8 @@
+package class_notes_question;
+
+public class java1 {
+    public static void main(String[] args) {
+        System.out.println("hey there my name is bhavya");
+        System.out.print("I AM FROM FD SECTION");
+    }
+}
