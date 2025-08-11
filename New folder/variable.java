fe1268c802a41114e49c8b51470fee9eccbe5e6a
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-08-11 11:02:59 +0530

    Add files via upload

---
+++ New folder/variable.java
@@ -0,0 +1,30 @@
+package class_notes_question;
+import java.util.Scanner;
+public class variable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int f=sc.nextInt();
+        System.out.println(a+f);
+
+
+
+
+
+
+
+        float b = 34.5f;
+        long c= 32343433;
+        double d = 45.453;
+        boolean e = true;
+        String bhavGupta =" hi ";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(bhavGupta);
+
+
+    }
+}
