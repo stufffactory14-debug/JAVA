89a433d7ad1e85cc4cf5c84fe50ace4089cd94c7
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-08-12 08:39:11 +0530

    Add files via upload

---
+++ New folder/datatype.java
@@ -0,0 +1,25 @@
+package class_notes_question;
+
+public class datatype {
+    public static void main(String[] args) {
+        int a=1243;
+        String bhavya = "hey this is my code";
+        System.out.println(bhavya.charAt(2));
+
+
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        int g = 23,k=45;
+        float nj=45.65f, df=7854.7f;
+        char ka='b',kfs='d';
+        short fr=45,nf=43;
+
+
+    }
+}
