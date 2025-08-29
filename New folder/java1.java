78bd24134d483dff99b477eede48fb647d1f416f
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-08-29 20:00:40 +0530

    Delete New folder directory

--- New folder/java1.java
+++
@@ -1,8 +0,0 @@
-package class_notes_question;
-
-public class java1 {
-    public static void main(String[] args) {
-        System.out.println("hey there my name is bhavya");
-        System.out.print("I AM FROM FD SECTION");
-    }
-}
