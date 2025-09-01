f5750a4dcffc262cbb306cea6c1f5a15cb56b061
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-09-01 22:33:32 +0530

    Update quicksort.java

--- self_learning/divide_conquer/quicksort.java
+++ self_learning/divide_conquer/quicksort.java
@@ -7,9 +7,6 @@ public class quicksort {
 //        for(int num:arr){
 //            System.out.print(num+" ");
 //        }
-       int[] nums = {4,5,6,0,1,2,3};
-            int target = 3;
-        System.out.println(search(nums,target));
 
     }
     //PIVIT APPROCH
@@ -39,41 +36,5 @@ public class quicksort {
         arr[i]=temp;
         return i;
     }
-
-    static int search(int[] nums, int target) {
-            int start = 0;
-            int end = nums.length - 1;
-
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-
-                // Found the target
-                if (nums[mid] == target) {
-                    return mid;
-                }
-
-                // Determine which half is sorted
-                if (nums[start] <= nums[mid]) {
-                    // Left half is sorted
-                    if (nums[start] <= target && target < nums[mid]) {
-                        end = mid - 1; // Target is in the left half
-                    } else {
-                        start = mid + 1; // Target is in the right half
-                    }
-                } else {
-                    // Right half is sorted
-                    if (nums[mid] < target && target <= nums[start]) {
-                        end = mid + 1; // Target is in the right half
-                    } else {
-                        start = mid - 1; // Target is in the left half
-                    }
-                }
-            }
-
-            return -1; // Target not found
-        }
-
-
-
-
 }
+
