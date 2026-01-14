fc86873298ac90091c0b2ac2e5df3ac8fd510efd
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2026-01-14 22:34:13 +0530

    Create buildtree.java

---
+++ self_learning/TREES/revision/buildtree.java
@@ -0,0 +1,61 @@
+package binaryTree;
+
+public class buildtree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node buildtree(int[] nodes){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newnode= new Node(nodes[idx]);
+            newnode.left=buildtree(nodes);
+            newnode.right=buildtree(nodes);
+            return newnode;
+        }
+
+        public static void preorder(Node root){
+            if(root == null) return;
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root){
+            if(root == null) return;
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        public static void postorder(Node root){
+            if(root == null) return;
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+
+        }
+    }
+    static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+      //  BinaryTree tree = new BinaryTree();
+        Node root =BinaryTree.buildtree(nodes);
+        //System.out.println(root.data);
+       // BinaryTree.inorder(root);
+        BinaryTree.postorder(root);
+    }
+
+    }
+
