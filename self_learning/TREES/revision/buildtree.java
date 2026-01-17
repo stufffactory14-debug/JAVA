9a58ba3e7cea061f8ece3a61e42e198d56838d4e
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2026-01-17 16:32:50 +0530

    Add subtree check and comment out sum and diameter

--- self_learning/TREES/revision/buildtree.java
+++ self_learning/TREES/revision/buildtree.java
@@ -110,7 +110,22 @@ public class buildtree {
       //  BinaryTree.levelOrder(root);
          //  System.out.println(BinaryTree.heightOfTree(root));
         //  System.out.println(BinaryTree.countOfNodes(root));
-        System.out.println(BinaryTree.sumOfNodes(root));
+          //System.out.println(BinaryTree.sumOfNodes(root));
+        //System.out.println(BinaryTree.diameter(root));
+
+        Node root1 = new Node(1);
+        root1.left= new Node(2);
+        root1.right= new Node(3);
+        root1.left.left = new Node(4);
+        root1.left.right = new Node(5);
+        root1.right.left = new Node(6);
+        root1.right.right =new Node(7);
+
+        Node subRoot = new Node(2);
+        subRoot.left= new Node(4);
+        subRoot.right = new Node(5);
+
+        System.out.println(BinaryTree.isSubtree(root1,subRoot));
     }
     }
 
