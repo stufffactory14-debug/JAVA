e5049ea33dca4ce4344b22947baee215b25cf7b5
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2026-01-19 09:08:51 +0530

    Update buildtree.java

--- self_learning/TREES/revision/buildtree.java
+++ self_learning/TREES/revision/buildtree.java
@@ -181,17 +181,14 @@ public class buildtree {
                         max = Math.max(max,current.hd+1);
                     }
                 }
-
-
-
-
-
             }
             for(int i=min;i<=max;i++){
                 System.out.print(map.get(i).data + " ");
             }
             System.out.println();
         }
+
+        
 
         static void kthlvl(Node root,int level,int k){
             if(root == null){
@@ -204,6 +201,40 @@ public class buildtree {
             kthlvl(root.left,level+1,k);
             kthlvl(root.right,level+1,k);
         }
+
+
+        
+        public static boolean getPath(Node root, int n, ArrayList<Node> path){
+            if(root==null) return false;
+
+            path.add(root);
+            if(root.data == n) return true;
+
+            boolean foundLeft = getPath(root.left,n,path);
+            boolean foundRight = getPath(root.right,n,path);
+
+            if(foundLeft || foundRight) return true;
+
+            path.remove(path.size()-1);
+            return false;
+        }
+        public static int lca(Node root,int n1,int n2){
+            ArrayList<Node> path1 = new ArrayList<>();
+            ArrayList<Node> path2 = new  ArrayList<>();
+
+            getPath(root,n1,path1);
+            getPath(root,n2,path2);
+            int i=0;
+            for(;i<path1.size() && i<path2.size();i++){
+                if(path1.get(i)!=path2.get(i)){
+                    break;
+                }
+            }
+            Node lca = path1.get(i-1);
+            return lca.data;
+        }
+
+        
 
 
     }
